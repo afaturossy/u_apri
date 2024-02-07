8178757d19fe69bf78a8a166e3a4164ac9ddd282
ari-fatur <ari.arifatur.fatur@gmail.com>
2024-02-07 10:10:55 +0700

    perbaikan meta

---
+++ src/routes/kirim/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let value = ""
+
+    function handleOk() {
+        console.log(value)
+        let ke = encodeURIComponent(value)
+        let url = `/?dear=${ke}`
+        console.log(url)
+        window.open(url)
+    }
+</script>
+
+<div>
+    <h2 style="text-align: center; margin-bottom: 1rem; color: white; font-family: sans-serif">Helper URL
+        Undangan</h2>
+    <div style="display: flex; justify-content: center ; font-size: large">
+        <input bind:value placeholder="dear" style="text-align: center">
+        <button on:click={handleOk}>ok</button>
+    </div>
+</div>
+
+<style>
+
+</style>
